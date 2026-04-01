e11d928859f99c0053ffd1abfc66fd13fe24813a
Markos Kandylis <markos@kandylis.co.uk>
2026-04-01 09:01:01 +0100

    Added two Target groups a blue and a green

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,25 @@ resource "aws_lb" "this" {
 }
 
 resource "aws_lb_target_group" "this" {
-  name        = "${var.name}-tg"
+  name        = "${var.name}-tg-blue"
+  port        = var.container_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip" # Required for Fargate
+
+  health_check {
+    path     = var.health_check_path
+    port     = "traffic-port"
+    matcher  = "200"
+    interval = 30
+    timeout  = 5
+  }
+
+  tags = merge(var.tags, { Name = "${var.name}-tg" })
+}
+
+resource "aws_lb_target_group" "this" {
+  name        = "${var.name}-tg-green"
   port        = var.container_port
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
